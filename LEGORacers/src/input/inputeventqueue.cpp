d9355d05876fb0642332302d2cb94e23d1b6e64e
foxtacles <mail@csemmler.com>
2026-05-25 20:46:17 -0700

    Refactor code components/modules in LEGORacers (#368)
    * Refactor code components/modules in LEGORacers
    
    * Rename input event class

---
+++ LEGORacers/src/input/inputeventqueue.cpp
@@ -0,0 +1,125 @@
+#include "input/inputeventqueue.h"
+
+#include "golerror.h"
+
+#include <string.h>
+
+DECOMP_SIZE_ASSERT(InputEventQueue, 0x1c)
+DECOMP_SIZE_ASSERT(InputEventQueue::Event, 0x10)
+
+// FUNCTION: LEGORACERS 0x0044b6f0
+InputEventQueue::InputEventQueue()
+{
+	Initialize();
+}
+
+// FUNCTION: LEGORACERS 0x0044b730
+InputEventQueue::~InputEventQueue()
+{
+	Reset();
+}
+
+// FUNCTION: LEGORACERS 0x0044b740
+void InputEventQueue::Initialize()
+{
+	ClearQueue();
+	m_allocated = FALSE;
+	m_events = NULL;
+	m_capacity = 0;
+}
+
+// FUNCTION: LEGORACERS 0x0044b760
+LegoBool32 InputEventQueue::Allocate(LegoS32 p_capacity)
+{
+	Reset();
+
+	m_events = new Event[p_capacity];
+	if (m_events == NULL) {
+		GOL_FATALERROR(c_golErrorOutOfMemory);
+	}
+
+	::memset(m_events, 0, sizeof(Event) * p_capacity);
+	m_capacity = p_capacity;
+	m_allocated = TRUE;
+	return TRUE;
+}
+
+// FUNCTION: LEGORACERS 0x0044b7c0
+LegoBool32 InputEventQueue::Reset()
+{
+	if (m_allocated) {
+		if (m_events != NULL) {
+			delete[] m_events;
+		}
+
+		Initialize();
+	}
+
+	return TRUE;
+}
+
+// FUNCTION: LEGORACERS 0x0044b7f0
+InputEventQueue::Event* InputEventQueue::Dequeue()
+{
+	Event* event = &m_events[m_readPos];
+	if (m_size == 0) {
+		return NULL;
+	}
+
+	m_readPos += 1;
+	m_readPos %= m_capacity;
+	m_size -= 1;
+	return event;
+}
+
+// FUNCTION: LEGORACERS 0x0044b820
+InputEventQueue::Event* InputEventQueue::Enqueue(InputDevice* p_device, LegoU32 p_keyCode, LegoU32 p_arg3)
+{
+	Event* event = &m_events[m_writePos];
+	if (m_size == m_capacity) {
+		return NULL;
+	}
+
+	event->m_device = p_device;
+	event->m_keyCode = p_keyCode;
+	event->m_unk0x08 = p_arg3;
+	event->m_isRepeat = FALSE;
+	event->m_isPressed = FALSE;
+	m_writePos += 1;
+	m_writePos %= m_capacity;
+	m_size += 1;
+	return event;
+}
+
+// FUNCTION: LEGORACERS 0x0044b870
+void InputEventQueue::OnKeyDown(InputDevice* p_device, undefined4 p_keyCode, undefined4 p_arg3)
+{
+	Event* event = Enqueue(p_device, p_keyCode, p_arg3);
+	if (event != NULL) {
+		event->m_isPressed = TRUE;
+	}
+}
+
+// FUNCTION: LEGORACERS 0x0044b890
+void InputEventQueue::OnKeyUp(InputDevice* p_device, undefined4 p_keyCode, undefined4 p_arg3)
+{
+	Event* event = Enqueue(p_device, p_keyCode, p_arg3);
+}
+
+// FUNCTION: LEGORACERS 0x0044b8b0
+void InputEventQueue::OnKeyRepeat(InputDevice* p_device, undefined4 p_keyCode, undefined4 p_arg3)
+{
+	Event* event = Enqueue(p_device, p_keyCode, p_arg3);
+	if (event != NULL) {
+		event->m_isPressed = TRUE;
+		event->m_isRepeat = TRUE;
+	}
+}
+
+// FUNCTION: LEGORACERS 0x0044b8e0
+void InputEventQueue::ClearQueue()
+{
+	m_writePos = 0;
+	m_readPos = 0;
+	m_size = 0;
+}
